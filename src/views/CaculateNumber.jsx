c6ae6b579b03bac626d18d6015e637dc1d01157d
wanwisaMungchareonklang <S6419410021@sau.ac.th>
2025-04-01 12:27:00 +0700

    ก่อนแก้

--- src/views/CaculateNumber.jsx
+++ src/views/CaculateNumber.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Box, Typography, Button, Avatar, Link, TextField, FormControl, Menu } from "@mui/material";
 
 function CaculateNumber() {
   return (
-    <div>CaculateNumber</div>
+    <>
+    <Box sx={{ height: '100vh', display: 'flex' }}>
+        <Box sx={{ width: '70%', boxShadow: 2, mx: 'auto', my: 'auto', p: 5 }}>
+            <Avatar src={'https://cdn-icons-png.flaticon.com/128/3845/3845843.png'} sx={{ width: 150, height: 150, mx: 'auto' }}/>
+            <Typography variant='h5' sx={{ textAlign: 'center', mt: 2 }}>
+                เครื่องคิดเลข
+            </Typography>
+            <Typography sx={{ mt: 2 }}>
+                ตัวเลขตัวที่1
+            </Typography>
+            <TextField variant='outlined' fullWidth type='number' />
+            <Typography sx={{ mt: 2 }}>
+                ตัวเลขตัวที่2
+            </Typography>
+            <TextField variant='outlined' fullWidth type='number' />
+            <Typography sx={{ mt: 2 }}>
+                เลือกเครื่องหมายคำนวณ
+            </Typography>
+            <FormControl fullWidth sx={{ mt: 2 }}/>
+            <select >
+                <MenuItem value={'+'}>+ บวก</MenuItem>
+                <MenuItem value={'-'}>+ ลบ</MenuItem>
+                <MenuItem value={'*'}>+ คูณ</MenuItem>
+                <MenuItem value={'/'}>+ หาร</MenuItem>
+
+            </select>
+        </Box>
+
+    </Box>
+    </>
   )
 }
 
